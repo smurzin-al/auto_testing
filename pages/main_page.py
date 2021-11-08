50ab4f22c675daf1b68119c86e904a97471f9794
Sergey Murzin <warka@1987@bk.ru>
2021-11-08 21:15:42 +0300

    add change pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
 
@@ -10,4 +9,6 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+
 
