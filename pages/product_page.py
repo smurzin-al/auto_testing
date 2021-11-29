9644b78ea6283e855fcdaa823052b78a8311249e
Sergey Murzin <warka@1987@bk.ru>
2021-11-29 19:57:38 +0300

    add parametrize test for basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,6 +15,11 @@ class ProductPage(BasePage):
     def button_add_to_basket_click(self):
         self.browser.find_element(*ProductPageLocators.BASKET_BUTTON).click()
 
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_VALUE).text
 
     def checking_basket(self):
         info_message_product_in_basket = (self.browser.find_element(
@@ -26,16 +31,17 @@ class ProductPage(BasePage):
             *ProductPageLocators.INFO_MESSAGE_BASKET_PRICE).text)
         assert "Стоимость корзины теперь составляет " in info_message_basket_price, \
             "No such message: Стоимость корзины теперь составляет"
-        self.compare_product_name()
-        self.compare_product_price()
 
-    def compare_product_name(self):
+        product_name = self.get_product_name()
+        product_price = self.get_product_price()
+        self.compare_product_name(product_name)
+        self.compare_product_price(product_price)
+
+    def compare_product_name(self, product_name):
         info_message_product_name = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE_PRODUCT_NAME).text
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         assert self.compare_values(info_message_product_name, product_name), "Wrong product's name in basket"
 
-    def compare_product_price(self):
-        info_message_product_price = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE_PRODUCT_PRICE_VALUE).text
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_VALUE).text
+    def compare_product_price(self, product_price):
+        info_message_product_price = self.browser.find_element(
+            *ProductPageLocators.INFO_MESSAGE_PRODUCT_PRICE_VALUE).text
         assert self.compare_values(info_message_product_price, product_price), "Wrong product's price in basket"
-
